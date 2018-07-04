333432083874684f6e8a6d1b12e4795e9e63a6ae
Adrien Bustany <adrien@bustany.org>
2018-07-04 22:37:50 +0200

    lib: Enable serde Serialize/Deserialize on model types

--- lib/build.rs
+++ lib/build.rs
@@ -1,6 +1,7 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["proto/flouze.proto"],
-                                &["proto/"]).unwrap();
+    let mut config = prost_build::Config::new();
+    config.type_attribute(".", "#[derive(Serialize, Deserialize)]");
+    config.compile_protos(&["proto/flouze.proto"], &["proto/"]).unwrap();
 }
