20a1773b1c426a841ffcc764a7ccd751aae5b903
Adrien Bustany <adrien@bustany.org>
2018-11-30 17:56:12 +0100

    app: Protect against potential concurrent accesses to the RPC client while it's initializing

--- mobile/lib/utils/rpc_client.dart
+++ mobile/lib/utils/rpc_client.dart
@@ -2,12 +2,13 @@ import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:flouze_flutter/flouze_flutter.dart';
 
-JsonRpcClient _cachedClient;
+Future<JsonRpcClient> _cachedClient;
 
 Future<JsonRpcClient> getJsonRpcClient() async {
   if (_cachedClient == null) {
-    String syncServerUrl = (await rootBundle.loadString('assets/sync_server_uri.txt')).trim();
-    _cachedClient = await JsonRpcClient.create(syncServerUrl);
+    _cachedClient = rootBundle.loadString('assets/sync_server_uri.txt')
+      .then((serverUri) => serverUri.trim())
+      .then((serverUri) => JsonRpcClient.create(serverUri));
   }
 
   return _cachedClient;
