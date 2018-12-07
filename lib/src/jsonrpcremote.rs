a28d25684116a015b41779bf4239663b7cddc08b
Adrien Bustany <adrien@bustany.org>
2018-12-07 16:05:09 +0100

    server: Add logging for RPC calls

--- lib/src/jsonrpcremote.rs
+++ lib/src/jsonrpcremote.rs
@@ -1,10 +1,13 @@
 use std::cell::RefCell;
 use std::net::SocketAddr;
 use std::ops::{Deref, DerefMut};
+use std::time::{Duration, Instant};
 use std::sync::RwLock;
 
 use jsonrpc_client_http;
 use jsonrpc_core;
+use jsonrpc_core::futures::Future;
+use jsonrpc_core::futures::future::Either;
 use jsonrpc_http_server;
 
 use uuid::Uuid;
@@ -187,6 +190,65 @@ impl<T: Repository + Send + Sync + 'static> server::Rpc for ServerRpcImpl<T> {
     }
 }
 
+#[derive(Clone, Debug, Default)]
+struct RequestMeta{}
+
+impl jsonrpc_core::Metadata for RequestMeta {}
+
+#[derive(Default)]
+struct LoggingMiddleware{}
+
+fn log_request_start(call: &jsonrpc_core::Call) {
+    if let jsonrpc_core::Call::MethodCall(method_call) = call {
+        info!("RPC IN  id={:?} method={}", &method_call.id, &method_call.method);
+    }
+}
+
+fn log_request_end(output: &jsonrpc_core::Output, request_time: &Duration) {
+    let request_time_ms = 1000 * request_time.as_secs() + request_time.subsec_millis() as u64;
+    let success = match output {
+        jsonrpc_core::Output::Success(_) => true,
+        _ => false,
+    };
+    info!("RPC OUT id={:?} elapsed={}ms success={}", output.id(), request_time_ms, success);
+}
+
+impl jsonrpc_core::Middleware<RequestMeta> for LoggingMiddleware {
+    type Future = jsonrpc_core::FutureResponse;
+    type CallFuture = jsonrpc_core::middleware::NoopCallFuture;
+
+    fn on_request<F, X>(&self, request: jsonrpc_core::Request, meta: RequestMeta, next: F) -> Either<Self::Future, X> where
+        F: FnOnce(jsonrpc_core::Request, RequestMeta) -> X + Send,
+        X: Future<Item=Option<jsonrpc_core::Response>, Error=()> + Send + 'static,
+    {
+        let start = Instant::now();
+
+        match request {
+            jsonrpc_core::Request::Single(ref call) => log_request_start(call),
+            jsonrpc_core::Request::Batch(ref calls) => {
+                for call in calls {
+                    log_request_start(call);
+                }
+            }
+        }
+
+        Either::A(Box::new(next(request, meta).map(move |res| {
+            let request_duration = start.elapsed();
+            match res {
+                Some(jsonrpc_core::Response::Single(ref output)) => log_request_end(output, &request_duration),
+                Some(jsonrpc_core::Response::Batch(ref outputs)) => {
+                    for output in outputs {
+                        log_request_end(output, &request_duration);
+                    }
+                }
+                _ => {}
+            }
+
+            res
+        })))
+    }
+}
+
 pub struct Server {
     server: jsonrpc_http_server::Server,
 }
@@ -196,7 +258,7 @@ impl Server {
         use self::server::*;
 
         let rpc_impl = ServerRpcImpl{repo: RwLock::new(repo)};
-        let mut io = jsonrpc_core::IoHandler::new();
+        let mut io = jsonrpc_core::MetaIoHandler::with_middleware(LoggingMiddleware::default());
         io.extend_with(rpc_impl.to_delegate());
 
         let addr: SocketAddr = listen_address.parse()?;
