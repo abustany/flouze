1dcf9be2c918ee31955656a17436c6983e184c80
Adrien Bustany <adrien@bustany.org>
2019-05-24 00:37:55 +0200

    app: Use rxdart's BehaviorSubject instead of manual caching in AccountListBloc

--- mobile/lib/blocs/account_list.dart
+++ mobile/lib/blocs/account_list.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
+
 import 'package:flouze/utils/services.dart' as Services;
 import 'package:flouze_flutter/flouze_flutter.dart' as Flouze;
 
 class AccountListBloc {
   var _repository = Services.getRepository();
-  var _accountsController = StreamController<AccountListState>.broadcast();
+  var _accountsController = BehaviorSubject<AccountListState>();
   var _notificationController = StreamController<String>.broadcast();
-  var _accounts = <Flouze.Account>[];
 
   StreamSubscription<String> _accountEvents;
 
@@ -28,15 +29,15 @@ class AccountListBloc {
     _repository
         .then((repo) => repo.listAccounts())
         .then((accounts) {
-          _accounts = accounts;
           _accountsController.add(AccountListLoadedState(accounts));
         })
         .catchError((e) => _accountsController.add(AccountListErrorState("Error while loading accounts: ${e.toString()}")));
   }
 
   void saveAccount(Flouze.Account account) {
-    _accounts.add(account);
-    _accountsController.add(AccountListLoadedState(_accounts));
+    final currentAccounts = (_accountsController.value is AccountListLoadedState) ?
+      (_accountsController.value as AccountListLoadedState).accounts : [];
+    _accountsController.add(AccountListLoadedState([...currentAccounts, account]));
 
     _repository
         .then((repo) => repo.addAccount(account))
