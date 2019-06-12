8ad88d58601012178df38cea2b340c84123a234f
Adrien Bustany <adrien@bustany.org>
2019-06-12 12:28:08 +0200

    app: Migrate the account clone page to the BLoC pattern

---
+++ mobile/lib/blocs/account_clone.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+
+import 'package:flouze/utils/account_config.dart';
+import 'package:flouze/utils/account_config_store.dart' as AccountConfigStore;
+import 'package:flouze/utils/rpc_client.dart';
+import 'package:flouze/utils/services.dart' as Services;
+import 'package:flouze/utils/uuid.dart' as UUID;
+import 'package:flouze_flutter/flouze_flutter.dart' as Flouze;
+
+class AccountCloneBloc {
+  var _repository = Services.getRepository();
+  var _accountsController = BehaviorSubject<AccountCloneState>();
+
+  Future<List<Flouze.Account>> _loadAccountList() =>
+      _repository.then((repo) => repo.listAccounts());
+
+  Future<Flouze.Account> _loadRemoteAccount(List<int> accountUuid) =>
+      getJsonRpcClient()
+          .then((client) => client.getAccountInfo(accountUuid));
+
+  void prepareImport(List<int> remoteAccountUuid) {
+    _accountsController.add(AccountCloneLoadingState());
+    Future.wait([_loadAccountList(), _loadRemoteAccount(remoteAccountUuid)])
+      .then((ctx) {
+        final List<Flouze.Account> accounts = ctx[0];
+        final Flouze.Account remoteAccount = ctx[1];
+
+        if (accounts.indexWhere((a) => UUID.uuidEquals(a.uuid, remoteAccount.uuid)) != -1) {
+          _accountsController.add(AccountCloneAlreadyExistsState(remoteAccount));
+        } else {
+          _accountsController.add(AccountCloneReadyState(remoteAccount));
+        }
+      })
+      .catchError((e) => _accountsController.add(AccountCloneErrorState("Error while preparing import: ${e.toString()}")));
+  }
+
+  void import(Flouze.Account remoteAccount, List<int> meUuid) {
+    final accountConfig = AccountConfig((b) {
+      b.synchronized = true;
+
+      if (meUuid != null && meUuid.isNotEmpty) {
+        b.meUuid.addAll(meUuid);
+      }
+    });
+
+    // We save the account config first, if that fails the account itself will
+    // not be saved, and the user will try again. If account saving fails, we'll
+    // leave behind a stray account config, not a huge deal.
+    _accountsController.add(AccountCloneCloningState(remoteAccount));
+
+    Future.wait(<Future<dynamic>>[
+      AccountConfigStore.saveAccountConfig(remoteAccount.uuid, accountConfig),
+      _repository,
+      getJsonRpcClient(),
+    ])
+    .then((ctx) {
+      final Flouze.SledRepository repository = ctx[1];
+      final Flouze.JsonRpcClient client = ctx[2];
+
+      return Flouze.Sync.cloneRemote(repository, client, remoteAccount.uuid);
+    })
+    .then((_) => _accountsController.add(AccountCloneDoneState()))
+    .catchError((e) => _accountsController.add(AccountCloneErrorState("Error while importing: ${e.toString()}")));
+  }
+
+  Stream<AccountCloneState> get accounts => _accountsController.stream;
+
+  void dispose() {
+    _accountsController.close();
+  }
+}
+
+class AccountCloneState {}
+
+class AccountCloneLoadingState extends AccountCloneState {}
+
+class AccountCloneAlreadyExistsState extends AccountCloneState {
+  AccountCloneAlreadyExistsState(this.remoteAccount);
+  final Flouze.Account remoteAccount;
+}
+
+class AccountCloneReadyState extends AccountCloneState {
+  AccountCloneReadyState(this.remoteAccount);
+  final Flouze.Account remoteAccount;
+}
+
+class AccountCloneCloningState extends AccountCloneState {
+  AccountCloneCloningState(this.remoteAccount);
+  final Flouze.Account remoteAccount;
+}
+
+class AccountCloneDoneState extends AccountCloneState {}
+
+class AccountCloneErrorState extends AccountCloneState{
+  AccountCloneErrorState(this.error);
+  final String error;
+}
--- mobile/lib/pages/account_clone.dart
+++ mobile/lib/pages/account_clone.dart
@@ -1,15 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'package:flouze_flutter/flouze_flutter.dart';
-
-import 'package:flouze/utils/account_config.dart';
-import 'package:flouze/utils/account_config_store.dart' as AccountConfigStore;
-import 'package:flouze/utils/services.dart';
-import 'package:flouze/utils/rpc_client.dart';
-import 'package:flouze/utils/uuid.dart';
+import 'package:flouze/blocs/account_clone.dart';
 
 class AccountClonePage extends StatefulWidget {
   final List<int> accountUuid;
@@ -23,108 +16,32 @@ class AccountClonePage extends StatefulWidget {
 class AccountClonePageState extends State<AccountClonePage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   final List<int> accountUuid;
-  List<Account> _accounts;
-  Account _remoteAccountInfo;
-  bool _cloning = false;
+
+  AccountCloneBloc _bloc;
+  StreamSubscription<AccountCloneState> _accountEvents;
 
   AccountClonePageState(this.accountUuid);
 
-  Future<void> loadAccounts() async {
-    try {
-      print('Listing accounts');
-      final SledRepository repository = await getRepository();
-      List<Account> accounts = await repository.listAccounts();
+  @override
+  void initState() {
+    _bloc = AccountCloneBloc();
 
-      if (mounted) {
-        setState(() {
-          _accounts = accounts ?? [];
-        });
+    _accountEvents = _bloc.accounts.listen((state) {
+      if (state is AccountCloneDoneState) {
+        Navigator.of(context).pop();
       }
-    } on PlatformException catch (e) {
-      print('Error while listing accounts: ${e.message}');
-    }
-  }
+    });
 
-  Future<void> loadRemoteAccountInfo() async {
-    try {
-      final client = await getJsonRpcClient();
-      final remoteAccountInfo = await client.getAccountInfo(accountUuid);
+    _bloc.prepareImport(accountUuid);
 
-      if (mounted) {
-        setState(() {
-          _remoteAccountInfo = remoteAccountInfo;
-        });
-      }
-    } on PlatformException catch (e) {
-      print('Error while retrieving remote account info: ${e.message}');
-    }
+    super.initState();
   }
 
   @override
-  void initState() {
-    super.initState();
-    loadAccounts();
-    loadRemoteAccountInfo();
-  }
-
-  void _import() async {
-    final navigator = Navigator.of(context);
-
-    try {
-      setState(() {
-        _cloning = true;
-      });
-
-      final repository = await getRepository();
-      final client = await getJsonRpcClient();
-      await Sync.cloneRemote(repository, client, accountUuid);
-
-      final accountConfig = AccountConfig((b) => b..synchronized = true);
-      await AccountConfigStore.saveAccountConfig(accountUuid, accountConfig);
-
-      if (mounted) {
-        navigator.pop();
-      }
-    } on PlatformException catch (e) {
-      print('Error while cloning account: ${e.message}');
-
-      setState(() {
-        _cloning = false;
-      });
-    }
-  }
-
-  Widget mainWidget() {
-    if (_accounts == null || _remoteAccountInfo == null) {
-      return Center(child: CircularProgressIndicator());
-    }
-
-    if (_accounts.indexWhere((a) => uuidEquals(a.uuid, accountUuid)) != -1) {
-      return Text('This account is already imported');
-    }
-
-    return Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text('Ready to import account: ${_remoteAccountInfo.label}'),
-          ],
-        ),
-        SizedBox(height: 8.0),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _cloning ?
-              CircularProgressIndicator() :
-              RaisedButton(
-                onPressed: _import,
-                child: Text('Import')
-              )
-          ],
-        )
-      ],
-    );
+  void dispose() {
+    _accountEvents.cancel();
+    _bloc.dispose();
+    super.dispose();
   }
 
   @override
@@ -137,21 +54,82 @@ class AccountClonePageState extends State<AccountClonePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: new EdgeInsets.all(16.0),
-            child: Text(
-              'Import a remote account',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
           Expanded(
             child: Padding(
-              padding: new EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
-              child: mainWidget()
+              padding: new EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+              child: StreamBuilder<AccountCloneState>(
+                stream: _bloc.accounts,
+                initialData: AccountCloneLoadingState(),
+                builder: (context, snapshot) {
+                  if (snapshot.data is AccountCloneLoadingState) {
+                    return _buildLoading();
+                  }
+
+                  if (snapshot.data is AccountCloneAlreadyExistsState) {
+                    return _buildAlreadyExists(snapshot.data);
+                  }
+
+                  if (snapshot.data is AccountCloneReadyState) {
+                    return _buildReady(snapshot.data);
+                  }
+
+                  if (snapshot.data is AccountCloneCloningState) {
+                    return _buildCloning(snapshot.data);
+                  }
+
+                  if (snapshot.data is AccountCloneErrorState) {
+                    return _buildError(snapshot.data);
+                  }
+
+                  return Container();
+                },
+              )
             )
           )
         ]
       ),
     );
   }
+
+  Widget _buildLoading() => Center(
+    child: Column(
+      children: <Widget>[
+        CircularProgressIndicator(key: Key('account-clone-loading')),
+        Text('Retrieving remote account information...')
+      ]
+    )
+  );
+
+  Widget _buildAlreadyExists(AccountCloneAlreadyExistsState state) => Center(
+    child: Text(
+     'You already have the account "${state.remoteAccount.label}" on your device',
+     key: Key('account-clone-already-exists')
+   )
+  );
+
+  Widget _buildReady(AccountCloneReadyState state) => Column(
+    children: <Widget>[
+      Text('Ready to import account ${state.remoteAccount.label}', key: Key('account-clone-ready-label')),
+      Center(
+        child: RaisedButton(
+          key: Key('account-clone-ready-import'),
+          child: Text('Import'),
+          // FIXME: Allow picking meUuid here
+          onPressed: () { _bloc.import(state.remoteAccount, null); }
+        )
+      ),
+    ],
+  );
+
+  Widget _buildCloning(AccountCloneCloningState state) => Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      CircularProgressIndicator(),
+      Text('Importing account ${state.remoteAccount.label}...', key: Key('account-clone-importing-label'))
+    ],
+  );
+
+  Widget _buildError(AccountCloneErrorState state) => Center(
+    child: Text(state.error),
+  );
 }
