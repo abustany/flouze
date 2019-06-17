fd3e38553aed72be6a7009b2b1b7411c63985740
Adrien Bustany <adrien@bustany.org>
2019-06-17 15:04:49 +0200

    flouze_flutter: Add a script to build the Rust library for iOS

---
+++ flouze_flutter/build-ios-libs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Builds the Rust part of the Flouze flutter plugin as a universal library and
+# installs it in ios/Classes/lib.
+
+set -e
+
+cd $(dirname $0)
+
+CARGO_ARGS=
+LIB_DIR=debug
+
+if [ "$1" == "--release" ]; then
+	CARGO_ARGS="${CARGO_ARGS} --release"
+	LIB_DIR=release
+fi
+
+cargo lipo -p flouze-flutter $CARGO_ARGS
+cp ../target/universal/$LIB_DIR/libflouze_flutter.a ios/Classes/lib/libflouze_flutter_rust.a
