6c761562811473c6e8b78a75be86ceff0a1e9e62
Adrien Bustany <adrien@bustany.org>
2019-06-18 18:54:44 +0200

    app: Ensure adb is running as root before running integration tests

--- mobile/run-integration-tests.sh
+++ mobile/run-integration-tests.sh
@@ -7,6 +7,10 @@ cd $(dirname $0)
 # Ensure we have flouze-cli available
 cargo build -p flouze-cli
 
+# Ensure adb is running as root, since this is needed for reverse port
+# forwarding.
+adb root
+
 # First test will create an account, populate and upload it to the test server
 flutter -d android drive test_driver/create_populate_share.dart
 
