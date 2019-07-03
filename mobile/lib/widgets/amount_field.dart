b574e8a4636841b6d44c8fcaea1a81b716a2d4cb
Adrien Bustany <adrien@bustany.org>
2019-07-03 16:58:17 +0200

    app: Redesign AmountField widget

--- mobile/lib/widgets/amount_field.dart
+++ mobile/lib/widgets/amount_field.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'package:flouze/utils/amounts.dart';
 import 'package:flouze/utils/config.dart';
 
 class AmountField extends StatelessWidget {
   final TextEditingController controller;
+  final String label;
   final bool notNull;
   final FocusNode _focusNode = FocusNode();
 
-  AmountField({Key key, this.controller, this.notNull = false}) : super(key: key) {
+  AmountField({Key key, this.controller, this.label, this.notNull = false}) : super(key: key) {
     _focusNode.addListener(() {
       if (_focusNode.hasFocus) {
         this.controller.selection = TextSelection(baseOffset: 0, extentOffset: this.controller.text.length);
@@ -18,36 +18,31 @@ class AmountField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) =>
-      Row(
-          children: <Widget>[
-            Expanded(
-                child: TextFormField(
-                  textAlign: TextAlign.end,
-                  keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true),
-                  controller: controller,
-                  decoration: const InputDecoration(
-                    hintText: '0',
-                  ),
-                  focusNode: _focusNode,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return notNull ? 'Amount cannot be empty' : null;
-                    }
+      TextFormField(
+        textAlign: TextAlign.end,
+        keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true),
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: '0',
+          labelText: label,
+          suffixText: AppConfig.currencySymbol,
+        ),
+        focusNode: _focusNode,
+        validator: (value) {
+          if (value.isEmpty) {
+            return notNull ? 'Amount cannot be empty' : null;
+          }
 
-                    final double numVal = double.tryParse(value.replaceFirst(String.fromCharCode(AppConfig.decimalSeparator), '.'));
+          final double numVal = double.tryParse(value.replaceFirst(String.fromCharCode(AppConfig.decimalSeparator), '.'));
 
-                    if (numVal == null) {
-                      return 'Amount is not a valid number';
-                    }
+          if (numVal == null) {
+            return 'Amount is not a valid number';
+          }
 
-                    if (notNull && numVal == 0) {
-                      return 'Amount should be greater than 0';
-                    }
-                  },
-                )
-            ),
-            SizedBox(width: 12.0),
-            Text(AppConfig.currencySymbol)
-          ]);
+          if (notNull && numVal == 0) {
+            return 'Amount should be greater than 0';
+          }
+        },
+      );
 
 }
