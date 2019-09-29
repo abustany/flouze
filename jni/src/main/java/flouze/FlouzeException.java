fc7798b440dcabfa07a97ede23185c69cfa2ee31
Adrien Bustany <adrien@bustany.org>
2019-09-29 16:37:10 +0200

    Remove JNI crate
    We don't use it anywhere, flouze_flutter has its own bindings.

--- jni/src/main/java/flouze/FlouzeException.java
+++
@@ -1,7 +0,0 @@
-package flouze;
-
-public class FlouzeException extends Exception {
-    public FlouzeException(String message) {
-        super(message);
-    }
-}
--- jni/src/main/java/flouze/Repository.java
+++
@@ -1,8 +0,0 @@
-package flouze;
-
-import flouze.Model.Account;
-
-interface Repository {
-    void addAccount(Account account);
-    Account getAccount(byte[] accountId);
-}
