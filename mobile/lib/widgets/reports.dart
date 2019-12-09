297957a67293141067505a03e7f1963902e47787
Adrien Bustany <adrien@bustany.org>
2019-12-10 00:32:28 +0100

    mobile: Don't show transfers column if transfer list is empty

--- mobile/lib/widgets/reports.dart
+++ mobile/lib/widgets/reports.dart
@@ -15,7 +15,12 @@ class Reports extends StatelessWidget {
   final Map<List<int>, int> balance;
   final List<Transfer> transfers;
 
-  Reports({Key key, @required this.members, @required this.balance, @required this.transfers}) : super(key: key);
+  Reports(
+      {Key key,
+      @required this.members,
+      @required this.balance,
+      @required this.transfers})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,40 +38,50 @@ class Reports extends StatelessWidget {
                 return TableRow(children: [
                   Padding(
                       padding: EdgeInsets.all(12.0),
-                      child: Text(personName(members, entry.key), key: subkey(key, '-$i-name'), textAlign: TextAlign.right)
-                  ),
+                      child: Text(personName(members, entry.key),
+                          key: subkey(key, '-$i-name'),
+                          textAlign: TextAlign.right)),
                   Padding(
                       padding: EdgeInsets.all(12.0),
-                      child: Text('${amountToString(entry.value)} ${AppConfig.currencySymbol}', key: subkey(key, '-$i-balance'))
-                  )
+                      child: Text(
+                          '${amountToString(entry.value)} ${AppConfig.currencySymbol}',
+                          key: subkey(key, '-$i-balance')))
                 ]);
               }).toList(),
-            )
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(child: Text(FlouzeLocalizations.of(context).howToSettle), padding: EdgeInsets.only(top: 24)),
-            Table(
-              children: [...transfers.map((entry) {
-                j++;
-                final debitorName = personName(members, entry.debitor);
-                final creditorName = personName(members, entry.creditor);
+            )),
+        if (transfers.isNotEmpty)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  child: Text(FlouzeLocalizations.of(context).howToSettle),
+                  padding: EdgeInsets.only(top: 24)),
+              Table(
+                children: [
+                  ...transfers.map((entry) {
+                    j++;
+                    final debitorName = personName(members, entry.debitor);
+                    final creditorName = personName(members, entry.creditor);
 
-                return TableRow(children: [
-                  Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text(FlouzeLocalizations.of(context).xOwesY(debitorName, creditorName), key: subkey(key, '-$i-owes'), textAlign: TextAlign.right)
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text('${amountToString(entry.amount.toInt())} ${AppConfig.currencySymbol}', key: subkey(key, '-$i-amount'))
-                  )
-                ]);
-              })],
-            )
-          ],
-        ),
+                    return TableRow(children: [
+                      Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                              FlouzeLocalizations.of(context)
+                                  .xOwesY(debitorName, creditorName),
+                              key: subkey(key, '-$i-owes'),
+                              textAlign: TextAlign.right)),
+                      Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                              '${amountToString(entry.amount.toInt())} ${AppConfig.currencySymbol}',
+                              key: subkey(key, '-$i-amount')))
+                    ]);
+                  })
+                ],
+              )
+            ],
+          ),
       ],
     );
   }
